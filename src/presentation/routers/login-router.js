cc33062fb6add68dd94ec2b80590cc2ef96ce27c
Daniel Sousa <danieldelphi2013@gmail.com>
2020-11-11 12:52:16 -0300

    Move error 401 to helpers method

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -18,8 +18,6 @@ class LoginRouter {
             return HttpResponse.badRequest('password')
         }
         this.authUseCase.auth(email, password)
-        return {
-            statusCode: 401
-        }
+        return HttpResponse.unauthorizedError()
     }
 }
