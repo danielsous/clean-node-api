9ee84b08cf37c45d7e9c178ee1fe03a86855725b
Daniel Sousa <danieldelphi2013@gmail.com>
2020-11-11 13:00:23 -0300

    Ensure LoginRouter returns 500 if an invalid AuthUseCase is provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -7,7 +7,8 @@ class LoginRouter {
     }
 
     route (httpRequest) {
-        if (!httpRequest || !httpRequest.body) {
+        if (!httpRequest || !httpRequest.body || !this.authUseCase
+            || !this.authUseCase.auth) {
             return HttpResponse.serverError()
         }
         const { email, password } = httpRequest.body;
